14d970ac7e6472ffda3db1e8a2d4adde83e4b269
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-06-07 16:24:42 -0500

    Update [action].js

--- pages/api/user/[action].js
+++ pages/api/user/[action].js
@@ -1,13 +1,44 @@
-export default (req, res) => {
+import { connectToDatabase } from "../../../util/mongodb";
+import bcrypt from "bcrypt";
+import { SendEmail } from "../../../util/sender";
+
+export default async (req, res) => {
+  // pulls out the action from the query
   const {
     query: { action },
   } = req;
 
+  //deletes confirm password from the user object
+  delete req.body.confirm_password;
+  const user = req.body;
+
+  //connects to the database so we can perform tasks on it
+  const { db } = await connectToDatabase();
+  const Users = await db.collection("users");
+
   switch (action) {
     case "signup":
-      //sign user up
-      res.send(200);
-      res.end();
+      //encrypt password
+      const encrypted_password = await bcrypt.hash(user.password, 10);
+      user.password = encrypted_password;
+      user.isVerified = false;
+
+      try {
+        //insert new user into the collection
+        const new_user = await Users.insertOne(user);
+        if (new_user.insertedCount === 1) {
+          //success
+          //SendEmail(user.email, "Testy Email", "Haha this worked");
+          res.status(200);
+          res.end();
+        }
+      } catch (error) {
+        //error
+        console.log(error);
+        res.status(500);
+        res.end();
+      }
+
       break;
 
     default:
