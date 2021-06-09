5b832020e3fb9557cd1e593286b5c65d74c166a9
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-06-09 15:39:23 -0500

    Update [user].js

--- pages/profile/[user].js
+++ pages/profile/[user].js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import styles from "../../styles/Home.module.css";
+import styles from "styles/Home.module.css";
 import { useSession } from "next-auth/client";
 
 function Profile({ user }) {
